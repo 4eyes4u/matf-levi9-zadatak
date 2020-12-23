8dbc97bb32da9b6be85eafeabf6806126c01858e
4eyes4u <kosta.grujcic@hotmail.com>
2020-12-24 00:53:31 +0100

    Refactoring.

---
+++ backend/app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3001;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Putting some products.
+products = new Map();
+products.set(0, {name: 'Panceta', description: 'Meso'});
+products.set(1, {name: 'Voda', description: 'Tecnost'});
+products.set(2, {name: 'Sok od pomorandze', description: 'Tecnost'});
+products.set(3, {name: 'Oreo', description: 'Grickalice'});
+products.set(4, {name: 'Pringles', description: 'Grickalice'});
+
+// Transform raw Map to JSON format.
+function productsToJSON() {
+    let data = {}
+    for ([key, value] of products) {
+        data[key] = value
+    }
+
+    return data;
+};
+
+app.get('/admin/proizvodi', (req, res) => {
+    res.send(productsToJSON());
+});
+
+app.post('/admin/unos-novog-proizvoda', (req, res) => {
+    // Reading data from body.
+    let productName = req.body.productName;
+    let productDescription = req.body.productDescription;
+
+    // Adding new product to Map.
+    products.set(products.size, {name: productName, description: productDescription});
+    res.send(productsToJSON());
+});
+
+app.delete('/admin/proizvodi', (req, res) => {
+    // Deleting proper product.
+    let key = req.body.productId;
+    products.delete(key);
+
+    // Returning remaining products.
+    data = productsToJSON();
+    res.send(data);
+});
+
+
+app.listen(port, () => {
+    console.log(`Listening on htpp://localhost:${port}`)
+})
